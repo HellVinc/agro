d5c796b5cefb1653e01dcc348438a3ad479188c3
iSkil <bondarenko.igor.97@gmail.com>
2017-07-20 20:56:00 +0300

    Creating a single tag in different categories

--- console/migrations/m170707_122346_create_tag_table.php
+++ console/migrations/m170707_122346_create_tag_table.php
@@ -15,13 +15,15 @@ class m170707_122346_create_tag_table extends Migration
         $this->createTable('tag', [
             'id' => $this->primaryKey(),
             'category_id' => $this->integer()->notNull(),
-            'name' => $this->string(255)->notNull()->unique(),
+            'name' => $this->string(255)->notNull(),
             'status' => $this->smallInteger(1)->defaultValue(10),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
             'created_by' => $this->integer(),
             'updated_by' => $this->integer(),
         ]);
+
+        $this->createIndex('unique_category_name', 'tag', ['category_id', 'name'], true);
     }
 
     /**
@@ -29,6 +31,7 @@ class m170707_122346_create_tag_table extends Migration
      */
     public function down()
     {
+        $this->dropIndex('unique_category_name', 'tag');
         $this->dropTable('tag');
     }
 }
