b95ecb22ce53b874a967a7923b957cab20f4500f
Igor Bondarenko <bondarenko.igor.97@gmail.com>
2017-09-12 14:37:09 +0300

    Change e-mail template

--- common/mail/newOffer-html.php
+++ common/mail/newOffer-html.php
@@ -6,11 +6,11 @@ use yii\helpers\Html;
 
 ?>
 <div class="offer">
-    <b>
-        <?= Html::encode($offer->creator->first_name . ' ' . $offer->creator->last_name) ?>,
+    <div>
+        <b><?= Html::encode($offer->creator->first_name . ' ' . $offer->creator->last_name) ?></b>,
         <?= Html::encode($offer->creator->Phone) ?>
-    </b>
-    <p><?= date('d.m.Y H:i', $offer->created_at) ?></p>
-    <hr>
+    </div>
+    <div><?= date('d.m.Y H:i', $offer->created_at) ?></div>
+    <hr size="1" noshade="">
     <pre><?= Html::encode($offer->text) ?></pre>
 </div>
